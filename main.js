dcf0d5b6630bbf655798e0021f20758a360326d7
juantraini <juantraini@hotmail.com>
2022-09-12 18:40:59 -0300

    arreglos

--- main.js
+++
@@ -1,7 +0,0 @@
-//Comportamiento barra lateral
-
-const menuLateral = document.querySelector('.menulateral');
-
-menuLateral.addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('active');
-});
